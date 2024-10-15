a77f224ea31522ef3bdcee132a8f689b791216f5
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2024-10-15 21:34:07 +0800

    removed unused variables

--- components/BlogContent.tsx
+++ components/BlogContent.tsx
@@ -1,7 +1,7 @@
 // "use client";
 
 import React from "react";
-import { Card, CardContent } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
