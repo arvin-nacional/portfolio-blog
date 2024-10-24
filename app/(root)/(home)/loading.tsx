93e923ff35d7ea96875484d543f81b8752d410c8
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2024-10-24 16:26:23 +0800

    added home loading page

---
+++ app/(root)/(home)/loading.tsx
@@ -0,0 +1,17 @@
+import About from "@/components/About";
+import CTA from "@/components/shared/CTA";
+import Hero2 from "@/components/Hero2";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      {/* <Hero /> */}
+      <Hero2 />
+      <About />
+      <CTA />
+    </div>
+  );
+};
+
+export default Page;
--- components/Hero2.tsx
+++ components/Hero2.tsx
@@ -24,14 +24,14 @@ const Hero2 = () => {
         <div>
           <div className="flex-between max-sm:flex-col max-sm:items-start">
             <div>
-              <h1 className="text-dark500_light700 h1-hero animate-fade-up font-extrabold leading-tight max-sm:text-[60px]">
+              <h1 className="text-dark500_light700 h1-hero font-extrabold leading-tight max-sm:text-[60px]">
                 Elevate your <br /> Brand
               </h1>
-              <h2 className="text-dark500_light700 max-sm:h2-semibold mt-2 animate-fade-up font-normal leading-tight animate-delay-500 sm:text-5xl lg:mb-12">
+              <h2 className="text-dark500_light700 max-sm:h2-semibold mt-2 font-normal leading-tight animate-delay-500 sm:text-5xl lg:mb-12">
                 Design, Develop, Dominate
               </h2>
             </div>
-            <div className="animate-fade-up animate-delay-[800ms] max-sm:mt-5 max-sm:w-[350px]">
+            <div className="max-sm:mt-5 max-sm:w-[350px]">
               <Image
                 src="/assets/images/hero-image.svg"
                 alt="hero-image"
@@ -44,13 +44,13 @@ const Hero2 = () => {
           <div className="flex-between mt-10 w-full flex-row items-start gap-5 max-sm:flex-col max-sm:items-start">
             <Link href="/contact">
               {" "}
-              <Button className="primary-gradient h2-semibold w-fit animate-fade-up cursor-pointer rounded-3xl px-9 py-6 font-light !text-light-900 animate-delay-[2000ms]">
+              <Button className="primary-gradient h2-semibold w-fit cursor-pointer rounded-3xl px-9 py-6 font-light !text-light-900 ">
                 {" "}
                 Connect with me
               </Button>
             </Link>
 
-            <div className=" ml-auto flex animate-fade-left gap-5 animate-delay-[2000ms]">
+            <div className=" ml-auto flex gap-5 ">
               <Image
                 src="/assets/icons/arrow-up-left-contained.svg"
                 width={80}
