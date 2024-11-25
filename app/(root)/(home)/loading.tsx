061c4a704ce6d057070525771592f3e80baa451a
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2024-11-25 23:08:07 +0800

    modified home loading page

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -2,23 +2,19 @@ import About from "@/components/About";
 import CTA from "@/components/shared/CTA";
 import Hero2 from "@/components/Hero2";
 import React from "react";
-import LogoAnimation from "@/components/LogoAnimation";
 import Services from "@/components/Services";
-import Testimonials from "@/components/Testimonials";
-import PortfolioSkeleton from "@/components/PortfolioSkeleton";
-import BlogsSkeleton from "@/components/BlogsSkeleton";
 
 const Page = () => {
   return (
     <div>
       {/* <Hero /> */}
       <Hero2 />
-      <LogoAnimation />
+      {/* <LogoAnimation /> */}
       <About />
       <Services />
-      <PortfolioSkeleton />
-      <BlogsSkeleton />
-      <Testimonials />
+      {/* <PortfolioSkeleton />
+      <BlogsSkeleton /> */}
+      {/* <Testimonials /> */}
       <CTA />
     </div>
   );
