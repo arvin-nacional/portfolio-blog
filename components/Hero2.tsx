137740bc464a7eca034c9344aa2fb37d7c03c9eb
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2025-01-27 20:33:05 +0800

    fixed padding

--- components/Hero2.tsx
+++ components/Hero2.tsx
@@ -19,8 +19,8 @@ const Hero2 = () => {
     //       ease: "easeInOut",
     //     }}
     //   >
-    <div className=" w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center h-screen">
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_60%,black)]"></div>
+    <div className=" w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center h-screen max-sm:px-8">
+      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_60%,black)] max-sm:[mask-image:radial-gradient(ellipse_at_center,transparent_75%,black)]"></div>
       <div className=" w-[1200px] max-w-full justify-between py-20 pb-6 max-md:mt-1 max-sm:py-2">
         <div>
           <div className="flex-between max-sm:flex-col max-sm:items-start">
